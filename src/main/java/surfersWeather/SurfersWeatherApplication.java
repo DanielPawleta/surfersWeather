69d501cd7be697aed66fdf67edcf0a0810cf79c6
dpawleta <tyla@op.pl>
2023-03-28 18:10:09 +0200

    Initial commit

---
+++ src/main/java/surfersWeather/SurfersWeatherApplication.java
@@ -0,0 +1,13 @@
+package surfersWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfersWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfersWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/surfersWeather/SurfersWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package surfersWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfersWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
